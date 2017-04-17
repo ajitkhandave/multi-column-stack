6038e0ac6e63f27903b46681a6e44253c7c151c5
ajitkhandave <ajitkhandave@gmail.com>
2017-04-17 17:56:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  public from:any;
+  public to:any;
+
+   constructor() {
+        this.options = {
+          chart:{type:'column'},
+
+            title : { text : 'Multi-column stacked bar chart' },
+
+            xAxis: {
+               categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+              },
+
+             yAxis: {
+                allowDecimals: false,
+                min: 0,
+                title: {
+                    text: 'Number of fruits'
+                }
+            },
+
+            plotOptions: {
+                column: {
+                    stacking: 'normal',
+                    point: {
+             events: {
+                       click: function() {
+                        console.log(this)
+                      }
+              }
+         }
+                }
+            },
+
+            credits: {
+                  enabled: false
+              },
+            series: [{
+                name: 'Ajit',
+                data: [20, 30, 40, 80],
+                stack: 'c1'
+            },
+            {
+                name: 'Amit',
+                data: [56, 46, 70, 69],
+                stack: 'c1'
+            },
+            {
+                name: 'Aditya',
+                data: [36, 80, 46, 40],
+                stack: 'c2'
+            },
+            {
+                name: 'Amar',
+                data: [68, 36, 89, 91],
+                stack: 'c2'
+            }]
+        };
+    }
+    options: Object;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { ChartModule } from 'angular2-highcharts';
 import { AppComponent } from './app.component';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+export function highchartsFactory() {
+  return require('highcharts');
+}
+var require: any;
 
 @NgModule({
   declarations: [
@@ -12,9 +17,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ChartModule
   ],
-  providers: [],
+ providers: [
+    {
+      provide: HighchartsStatic,
+      useFactory: highchartsFactory
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
